0d300f70ae6fcb7bc0881c58988aba3965e23cfd
EustacheElina <135215790+EustacheElina@users.noreply.github.com>
2024-12-11 14:03:18 +0100

    added plots

--- Firm two assets + fixed cost + default - Copy/_run.py
+++ Firm two assets + fixed cost + default - Copy/_run.py
@@ -10,19 +10,28 @@ from setup import *
 from vfi import *
 from nvfi import * 
 
-k_policy = np.ones((N_z, N_b, N_k)) * k_grid[np.newaxis,np.newaxis,:]
-b_policy = np.ones((N_z, N_b, N_k)) * b_grid[np.newaxis,:,np.newaxis]
 
 V_init = np.zeros((N_z, N_b, N_k))
-#V_vfi, k_policy_vfi, b_policy_vfi = vfi(V_init, beta, nu, psi, xi, delta, alpha, cf, r, P, z_grid, b_grid, k_grid)
+V_vfi, k_policy_vfi, b_policy_vfi = vfi(V_init, beta, nu, psi, xi, delta, alpha, cf, r, P, z_grid, b_grid, k_grid)
 V_nvfi, k_policy_nvfi, b_policy_nvfi = nvfi(V_init, beta, nu, psi, xi, delta, alpha, cf, r, P, z_grid, b_grid, k_grid, tol = 1e-5)
+
+exit_nvfi = (b_policy_nvfi > 0).astype(int)
+inaction_nvfi = (b_policy_nvfi == b_grid).astype(int)
+inaction_nvfi2 = (b_policy_nvfi == nu*k_policy_nvfi).astype(int)
 
 #plt.plot(k_grid, k_policy_vfi[0,0,:] - (1-delta)*k_grid, label = 'vfi')
 plt.plot(k_grid, k_policy_nvfi[0,0,:] - (1-delta)*k_grid, label = 'nvfi')
-
 plt.legend()
 plt.xlabel('k')
-plt.title('Policy function for iz = 0, ib  0')
+plt.title('Policy function for capital investment iz = 0, ib =0')
+plt.show()
+
+plt.plot(b_grid, b_policy_nvfi[0,:,10], label = 'nvfi')
+plt.plot(b_grid,nu*k_policy_nvfi[0,:,10])
+plt.legend()
+plt.xlabel('b')
+plt.ylabel('bprime ')
+plt.title('Policy function for debt issuance iz = 0, ik =10')
 plt.show()
 
 fig, axes = plt.subplots(1, 2, figsize=(12, 6))
@@ -55,9 +64,10 @@ axes[1].set_title('Debt choice with high capital')
 plt.tight_layout()
 plt.show()
 
+inaction_inv_nvfi = (k_policy_nvfi == (1-delta)*k_grid[np.newaxis,np.newaxis,:]).astype(int)
 coh = z_grid[:,np.newaxis,np.newaxis] * k_grid[np.newaxis,np.newaxis,:]**alpha + (1-delta) * k_grid[np.newaxis,np.newaxis,:] - b_grid[np.newaxis,:,np.newaxis] * (1+r) 
-adj_cost = psi / 2 * (k_policy - (1-delta) * k_grid[np.newaxis,np.newaxis,:])*2 / k_grid[np.newaxis,np.newaxis,:] + xi * k_grid[np.newaxis,np.newaxis,:]
-div_opt = (1-exit) * (coh - adj_cost - k_policy + b_policy - cf )
+adj_cost = psi / 2 * (k_policy_nvfi - (1-delta) * k_grid[np.newaxis,np.newaxis,:])*2 / k_grid[np.newaxis,np.newaxis,:] + xi * k_grid[np.newaxis,np.newaxis,:]
+div_opt = (1-exit) * (coh - adj_cost*(1-inaction_inv_nvfi) - k_policy_nvfi + b_policy_nvfi - cf )
 
 B_grid, K_grid = np.meshgrid(b_grid, k_grid)
 fig = plt.figure()
